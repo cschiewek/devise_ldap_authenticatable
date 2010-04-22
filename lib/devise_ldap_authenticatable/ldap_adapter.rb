80da6d52f7efd2163c9c80a79737360c6efe2e1e
Curtis Schiewek <curtis@MacBookPro.local>
2010-04-22 01:11:14 -0400

    first fully funtioning version

--- lib/devise_ldap_authenticatable/ldap_adapter.rb
+++ lib/devise_ldap_authenticatable/ldap_adapter.rb
@@ -8,14 +8,24 @@ module Devise
 
     def self.valid_credentials?(login, password)
       debugger
-      ldap = Net::LDAP.new( :host => ::Devise.ldap_host, :port => ::Devise.ldap_port )
-      if ldap.bind( :method => :simple, :username => login, :password => password )
+      #ldap = Net::LDAP.new( :host => ::Devise.ldap_host, :port => ::Devise.ldap_port )
+      #if ldap.bind( :method => :simple, :username => login, :password => password )
+      #  true
+      #else
+      #  false
+      #end
+      ldap = Net::LDAP.new
+      ldap.host = ::Devise.ldap_host
+      ldap.port = ::Devise.ldap_port
+      ldap.auth login, password
+      if ldap.bind
         true
       else
+        errors.add_to_base(ldap.get_operation_result.message)
         false
       end
     end
 
   end
-  
+
 end
