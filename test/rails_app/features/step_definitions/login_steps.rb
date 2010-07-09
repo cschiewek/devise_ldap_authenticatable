e73b086dfc6fe6cd14f54510212f7f68f3033cd7
Daniel McNevin <dpmcnevin@gmail.com>
2010-07-09 08:24:52 -0400

    added cucumber

---
+++ test/rails_app/features/step_definitions/login_steps.rb
@@ -0,0 +1,15 @@
+Given /^the following logins:$/ do |logins|
+  debugger
+  User.create(:email => logins, :password => password)
+end
+
+When /^I delete the (\d+)(?:st|nd|rd|th) login$/ do |pos|
+  visit logins_path
+  within("table tr:nth-child(#{pos.to_i+1})") do
+    click_link "Destroy"
+  end
+end
+
+Then /^I should see the following logins:$/ do |expected_logins_table|
+  expected_logins_table.diff!(tableish('table tr', 'td,th'))
+end
