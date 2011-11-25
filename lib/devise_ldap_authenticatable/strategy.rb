f5233643a8bea72abd8c153b5a89568b74f21da2
Curtis Schiewek <curtis.schiewek@greatersudbury.ca>
2011-11-25 11:07:08 -0500

    Refactored strategy to be more inline with how database_authenticatable functions.  Fixes #21 and Fixes #61

--- lib/devise_ldap_authenticatable/strategy.rb
+++ lib/devise_ldap_authenticatable/strategy.rb
@@ -5,30 +5,17 @@ module Devise
     # Strategy for signing in a user based on his login and password using LDAP.
     # Redirects to sign_in page if it's not authenticated
     class LdapAuthenticatable < Authenticatable
-      def valid?
-        valid_controller? && valid_params? && mapping.to.respond_to?(:authenticate_with_ldap)
-      end
-
       # Authenticate a user based on login and password params, returning to warden
       # success and the authenticated user if everything is okay. Otherwise redirect
       # to sign in page.
       def authenticate!
-        if resource = mapping.to.authenticate_with_ldap(params[scope])
+        resource = valid_password? && mapping.to.authenticate_with_ldap(params[scope])
+        if validate(resource)
           success!(resource)
         else
           fail(:invalid)
         end
-     end
-
-      protected
-
-        def valid_controller?
-          params[:controller] == mapping.controllers[:sessions]
-        end
-
-        def valid_params?
-          params[scope] && params[scope][:password].present?
-        end
+      end
     end
   end
 end
