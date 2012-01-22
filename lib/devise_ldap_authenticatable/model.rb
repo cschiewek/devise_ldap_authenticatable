abf7d1a08a9eeab1bea0b97e4acf1384d621b31a
Sven Steinheißer <sven.steinheisser@googlemail.com>
2012-01-22 14:31:15 +0100

    Respect Devise.case_insensitive_keys when ldap_create_user is set to true.

--- lib/devise_ldap_authenticatable/model.rb
+++ lib/devise_ldap_authenticatable/model.rb
@@ -21,7 +21,7 @@ module Devise
         @login_with ||= Devise.mappings[self.class.to_s.underscore.to_sym].to.authentication_keys.first
         self[@login_with]
       end
-      
+
       def reset_password!(new_password, new_password_confirmation)
         if new_password == new_password_confirmation && ::Devise.ldap_update_password
           Devise::LdapAdapter.update_password(login_with, new_password)
@@ -42,11 +42,11 @@ module Devise
           return false
         end
       end
-      
+
       def ldap_groups
         Devise::LdapAdapter.get_groups(login_with)
       end
-      
+
       def ldap_dn
         Devise::LdapAdapter.get_dn(login_with)
       end
@@ -67,34 +67,36 @@ module Devise
       module ClassMethods
         # Authenticate a user based on configured attribute keys. Returns the
         # authenticated user if it's valid or nil.
-        def authenticate_with_ldap(attributes={}) 
+        def authenticate_with_ldap(attributes={})
           auth_key = self.authentication_keys.first
-          return nil unless attributes[auth_key].present? 
+          return nil unless attributes[auth_key].present?
+
+          auth_key_value = (self.case_insensitive_keys || []).include?(auth_key) ? attributes[auth_key].downcase : attributes[auth_key]
 
           # resource = find_for_ldap_authentication(conditions)
-          resource = where(auth_key => attributes[auth_key]).first
-                    
+          resource = where(auth_key => auth_key_value).first
+
           if (resource.blank? and ::Devise.ldap_create_user)
             resource = new
-            resource[auth_key] = attributes[auth_key]
+            resource[auth_key] = auth_key_value
             resource.password = attributes[:password]
           end
-                    
+
           if resource.try(:valid_ldap_authentication?, attributes[:password])
             if resource.new_record?
               resource.ldap_before_save if resource.respond_to?(:ldap_before_save)
-              resource.save 
+              resource.save
             end
             return resource
           else
             return nil
           end
         end
-        
+
         def update_with_password(resource)
           puts "UPDATE_WITH_PASSWORD: #{resource.inspect}"
         end
-        
+
       end
     end
   end
--- test/rails_app/test/unit/user_test.rb
+++ test/rails_app/test/unit/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   def should_be_validated(user, password, message = "Password is invalid")
     assert(user.valid_ldap_authentication?(password), message)
   end
-  
+
   def should_not_be_validated(user, password, message = "Password is not properly set")
      assert(!user.valid_ldap_authentication?(password), message)
   end
@@ -25,12 +25,12 @@ class UserTest < ActiveSupport::TestCase
         assert_equal false, ::Devise::LdapAdapter.valid_login?('barneystinson')
       end
     end
-  
+
     context "create a basic user" do
       setup do
         @user = Factory(:user)
       end
-  
+
       should "check for password validation" do
         assert_equal(@user.email, "example.user@test.com")
         should_be_validated @user, "secret"
@@ -38,18 +38,18 @@ class UserTest < ActiveSupport::TestCase
         should_not_be_validated @user, "Secret"
       end
     end
-  
+
     context "change a LDAP password" do
       setup do
         @user = Factory(:user)
       end
-  
+
       should "change password" do
         should_be_validated @user, "secret"
         @user.reset_password!("changed","changed")
         should_be_validated @user, "changed", "password was not changed properly on the LDAP sevrer"
       end
-    
+
       should "not allow to change password if setting is false" do
         should_be_validated @user, "secret"
         ::Devise.ldap_update_password = false
@@ -58,42 +58,67 @@ class UserTest < ActiveSupport::TestCase
         should_be_validated @user, "secret"
       end
     end
-  
+
     context "create new local user if user is in LDAP" do
-    
+
       setup do
         assert(User.all.blank?, "There shouldn't be any users in the database")
       end
-    
+
       should "don't create user in the database" do
         @user = User.authenticate_with_ldap(:email => "example.user@test.com", :password => "secret")
         assert(User.all.blank?)
       end
-    
+
       context "creating users is enabled" do
         setup do
           ::Devise.ldap_create_user = true
         end
-      
+
         should "create a user in the database" do
           @user = User.authenticate_with_ldap(:email => "example.user@test.com", :password => "secret")
           assert_equal(User.all.size, 1)
           assert_contains(User.all.collect(&:email), "example.user@test.com", "user not in database")
         end
-  
+
         should "not create a user in the database if the password is wrong_secret" do
           @user = User.authenticate_with_ldap(:email => "example.user", :password => "wrong_secret")
           assert(User.all.blank?, "There's users in the database")
         end
-      
+
         should "create a user if the user is not in LDAP" do
           @user = User.authenticate_with_ldap(:email => "wrong_secret.user@test.com", :password => "wrong_secret")
           assert(User.all.blank?, "There's users in the database")
         end
+
+        should "create a user in the database if case insensitivity does not matter" do
+          ::Devise.case_insensitive_keys = false
+          @user = Factory(:user)
+
+          assert_difference "User.count", +1 do
+            User.authenticate_with_ldap(:email => "EXAMPLE.user@test.com", :password => "secret")
+          end
+        end
+
+        should "not create a user in the database if case insensitivity matters" do
+          ::Devise.case_insensitive_keys = [:email]
+          @user = Factory(:user)
+
+          assert_no_difference "User.count" do
+            User.authenticate_with_ldap(:email => "EXAMPLE.user@test.com", :password => "secret")
+          end
+        end
+
+        should "create a user with downcased email in the database if case insensitivity matters" do
+          ::Devise.case_insensitive_keys = [:email]
+
+          @user = User.authenticate_with_ldap(:email => "EXAMPLE.user@test.com", :password => "secret")
+          assert_contains(User.all.collect(&:email), "example.user@test.com", "user not in database")
+        end
       end
-    
+
     end
-  
+
     context "use groups for authorization" do
       setup do
         @admin = Factory(:admin)
@@ -101,55 +126,55 @@ class UserTest < ActiveSupport::TestCase
         ::Devise.authentication_keys = [:email]
         ::Devise.ldap_check_group_membership = true
       end
-  
+
       should "admin should be allowed in" do
         should_be_validated @admin, "admin_secret"
       end
-    
+
       should "admin should have the proper groups set" do
         assert_contains(@admin.ldap_groups, /cn=admins/, "groups attribute not being set properly")
       end
-    
+
       should "user should not be allowed in" do
         should_not_be_validated @user, "secret"
       end
-      
+
       should "not be validated if group with different attribute is removed" do
         `ldapmodify #{ldap_connect_string} -f ../ldap/delete_authorization_role.ldif`
         should_not_be_validated @admin, "admin_secret"
       end
     end
-  
+
     context "use role attribute for authorization" do
       setup do
         @admin = Factory(:admin)
         @user = Factory(:user)
         ::Devise.ldap_check_attributes = true
       end
-  
+
       should "admin should be allowed in" do
         should_be_validated @admin, "admin_secret"
       end
-    
+
       should "user should not be allowed in" do
         should_not_be_validated @user, "secret"
       end
     end
-    
+
     context "use admin setting to bind" do
       setup do
         @admin = Factory(:admin)
         @user = Factory(:user)
         ::Devise.ldap_use_admin_to_bind = true
       end
-  
+
       should "description" do
         should_be_validated @admin, "admin_secret"
       end
     end
-  
+
   end
-  
+
   context "use uid for login" do
     setup do
       default_devise_settings!
@@ -157,24 +182,24 @@ class UserTest < ActiveSupport::TestCase
       ::Devise.ldap_config = "#{Rails.root}/config/#{"ssl_" if ENV["LDAP_SSL"]}ldap_with_uid.yml"
       ::Devise.authentication_keys = [:uid]
     end
-  
+
     context "description" do
       setup do
         @admin = Factory(:admin)
         @user = Factory(:user, :uid => "example_user")
       end
-  
+
       should "be able to authenticate using uid" do
         should_be_validated @user, "secret"
         should_not_be_validated @admin, "admin_secret"
       end
     end
-    
+
     context "create user" do
       setup do
         ::Devise.ldap_create_user = true
       end
-  
+
       should "create a user in the database" do
         @user = User.authenticate_with_ldap(:uid => "example_user", :password => "secret")
         assert_equal(User.all.size, 1)
@@ -199,22 +224,22 @@ class UserTest < ActiveSupport::TestCase
           should_be_validated Factory(:user, :uid => "example_user"), "secret"
         end
       end
-    end    
+    end
   end
-  
+
   context "using ERB in the config file" do
     setup do
       default_devise_settings!
       reset_ldap_server!
       ::Devise.ldap_config = "#{Rails.root}/config/#{"ssl_" if ENV["LDAP_SSL"]}ldap_with_erb.yml"
     end
-  
+
     context "authenticate" do
       setup do
         @admin = Factory(:admin)
         @user = Factory(:user)
       end
-  
+
       should "be able to authenticate" do
         should_be_validated @user, "secret"
         should_be_validated @admin, "admin_secret"
@@ -235,7 +260,7 @@ class UserTest < ActiveSupport::TestCase
       end
     end
   end
-  
+
   context "use username builder" do
     setup do
       default_devise_settings!
@@ -250,5 +275,5 @@ class UserTest < ActiveSupport::TestCase
       should_be_validated @other, "other_secret"
     end
   end
-  
+
 end
