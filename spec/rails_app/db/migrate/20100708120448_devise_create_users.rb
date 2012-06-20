844181e72a675e18f5c97a7320bbb3eb088da7d1
Steven Xu <steven@stevenxu.ca>
2012-06-20 17:52:42 -0400

    updated migration and schema rb.

--- spec/rails_app/db/migrate/20100708120448_devise_create_users.rb
+++ spec/rails_app/db/migrate/20100708120448_devise_create_users.rb
@@ -1,10 +1,24 @@
 class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
-      t.database_authenticatable :null => false
-      t.recoverable
-      t.rememberable
-      t.trackable
+      ## Database authenticatable
+      t.string :email,              :null => false, :default => ""
+      t.string :encrypted_password, :null => false, :default => ""
+
+      ## Recoverable
+      t.string   :reset_password_token
+      t.datetime :reset_password_sent_at
+
+      ## Rememberable
+      t.datetime :remember_created_at
+
+      ## Trackable
+      t.integer  :sign_in_count, :default => 0
+      t.datetime :current_sign_in_at
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
+      t.string   :last_sign_in_ip
+
       t.string :uid
 
       # t.confirmable
