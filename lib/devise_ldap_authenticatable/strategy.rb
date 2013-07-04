6658e6904609f9d891b9ba88dd6145a666d42b3a
Curtis Schiewek <curtis.schiewek@gmail.com>
2013-07-04 14:50:04 -0400

    updated strategy to support http basic auth.  Fixes #80

--- lib/devise_ldap_authenticatable/strategy.rb
+++ lib/devise_ldap_authenticatable/strategy.rb
@@ -2,20 +2,13 @@ require 'devise/strategies/authenticatable'
 
 module Devise
   module Strategies
-    # Strategy for signing in a user based on his login and password using LDAP.
-    # Redirects to sign_in page if it's not authenticated
     class LdapAuthenticatable < Authenticatable
-      # Authenticate a user based on login and password params, returning to warden
-      # success and the authenticated user if everything is okay. Otherwise redirect
-      # to sign in page.
       def authenticate!
-        resource = valid_password? && mapping.to.authenticate_with_ldap(params[scope])
-        return fail(:invalid) if resource.nil?
+        resource = valid_password? && mapping.to.authenticate_with_ldap(authentication_hash.merge(password: password))
+        return fail(:invalid) unless resource
 
         if validate(resource)
           success!(resource)
-        else
-          fail(:invalid)
         end
       end
     end
