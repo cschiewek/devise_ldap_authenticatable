d0c54af9774e6b0c184e6935c77bfd235f2c63ed
Curtis Schiewek <curtis.schiewek@gmail.com>
2013-07-04 14:16:40 -0400

    removed empty modules

--- lib/devise_ldap_authenticatable.rb
+++ lib/devise_ldap_authenticatable.rb
@@ -3,10 +3,8 @@ require 'devise'
 
 require 'devise_ldap_authenticatable/exception'
 require 'devise_ldap_authenticatable/logger'
-require 'devise_ldap_authenticatable/schema'
 require 'devise_ldap_authenticatable/ldap/adapter'
 require 'devise_ldap_authenticatable/ldap/connection'
-require 'devise_ldap_authenticatable/routes'
 
 # Get ldap information from config/ldap.yml now
 module Devise
--- lib/devise_ldap_authenticatable/routes.rb
+++
@@ -1,8 +0,0 @@
-## No routes needed anymore since Devise.add_module with the :route parameter will take care of it.
-
-# ActionController::Routing::RouteSet::Mapper.class_eval do
-# 
-#   protected
-#     # reuse the session routes and controller
-#     alias :ldap_authenticatable :database_authenticatable
-# end
