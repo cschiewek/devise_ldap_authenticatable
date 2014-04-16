6a6f72906f40e154ee60bcc508bd7e3bc10a0e8d
Jake Champlin <jake.champlin.27@gmail.com>
2014-04-16 16:55:58 -0400

    Fixed a spelling error
    Took 10 minutes. Wow
    
    Signed-off-by: "Jake Champlin" <jake.champlin.27@gmail.com>

--- lib/devise_ldap_authenticatable/ldap/connection.rb
+++ lib/devise_ldap_authenticatable/ldap/connection.rb
@@ -18,7 +18,7 @@ module Devise
 
         @group_base = ldap_config["group_base"]
         @check_group_membership = ldap_config.has_key?("check_group_membership") ? ldap_config["check_group_membership"] : ::Devise.ldap_check_group_membership
-		  @check_group_membership_without_admin = ldap_config.has_key?("check_group_membership_without_admin") ? ldap_config["check_group_membership_without_admin"] : ::Devise.ldap_check_group_memebership_without_admin
+		  @check_group_membership_without_admin = ldap_config.has_key?("check_group_membership_without_admin") ? ldap_config["check_group_membership_without_admin"] : ::Devise.ldap_check_group_membership_without_admin
         @required_groups = ldap_config["required_groups"]
         @required_attributes = ldap_config["require_attribute"]
 
