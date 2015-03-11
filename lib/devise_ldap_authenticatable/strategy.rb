e6c2a821ff2dbafc8383637db8af8ecfcaa8a83b
Andrew Curley <andrew.curley@gmail.com>
2015-03-11 18:18:16 -0400

    Provide different fail message if the user provided invalid credentials and cannot be found in LDAP

--- lib/devise_ldap_authenticatable/strategy.rb
+++ lib/devise_ldap_authenticatable/strategy.rb
@@ -6,12 +6,26 @@ module Devise
       def authenticate!
         resource = mapping.to.find_for_ldap_authentication(authentication_hash.merge(password: password))
 
-        if resource && validate(resource) { resource.valid_ldap_authentication?(password) }
-          remember_me(resource)
-          resource.after_ldap_authentication
-          success!(resource)
-        else
-          return fail(:invalid)
+        # resource exists in database
+        if resource.persisted?
+          if validate(resource) { resource.valid_ldap_authentication?(password) }
+            remember_me(resource)
+            resource.after_ldap_authentication
+            success!(resource)
+          else
+            return fail(:invalid) # Invalid credentials
+          end
+        end
+
+        # resource does not exist in database
+        if resource.new_record?
+
+          if validate(resource) { resource.valid_ldap_authentication?(password) }
+            return fail(:not_found_in_database) # Valid credentials
+          else
+            return fail(:invalid) # Invalid credentials
+          end
+          
         end
       end
     end
