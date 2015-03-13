3312ecd9e759630df5df3da3216b52baf000f079
Andrew Curley <andrew.curley@gmail.com>
2015-03-13 00:27:45 -0400

    Write more detailed method documentation

--- lib/devise_ldap_authenticatable/strategy.rb
+++ lib/devise_ldap_authenticatable/strategy.rb
@@ -2,11 +2,16 @@ require 'devise/strategies/authenticatable'
 
 module Devise
   module Strategies
-    class LdapAuthenticatable < Authenticatable
+    class LdapAuthenticatable < Autheneticatable
+
+      # Tests whether the returned resource exists in the database and the
+      # credentials are valid.  If the resource is in the database and the credentials
+      # are valid, the user is authenticated.  Otherwise failure messages are returned 
+      # indicating whether the resource is not found in the database or the credentials
+      # are invalid.
       def authenticate!
         resource = mapping.to.find_for_ldap_authentication(authentication_hash.merge(password: password))
 
-        # resource exists in database
         if resource.persisted?
           if validate(resource) { resource.valid_ldap_authentication?(password) }
             remember_me(resource)
@@ -17,15 +22,12 @@ module Devise
           end
         end
 
-        # resource does not exist in database
         if resource.new_record?
-
           if validate(resource) { resource.valid_ldap_authentication?(password) }
             return fail(:not_found_in_database) # Valid credentials
           else
             return fail(:invalid) # Invalid credentials
           end
-          
         end
       end
     end
