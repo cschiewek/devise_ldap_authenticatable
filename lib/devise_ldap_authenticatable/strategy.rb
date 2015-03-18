d095fba81ee0accf8a7d0e3166c30145b48a8464
Rob White <robert.white@epigenesys.co.uk>
2015-03-18 16:16:31 +0000

    Fix for typo in constant name

--- lib/devise_ldap_authenticatable/strategy.rb
+++ lib/devise_ldap_authenticatable/strategy.rb
@@ -2,11 +2,11 @@ require 'devise/strategies/authenticatable'
 
 module Devise
   module Strategies
-    class LdapAuthenticatable < Autheneticatable
+    class LdapAuthenticatable < Authenticatable
 
       # Tests whether the returned resource exists in the database and the
       # credentials are valid.  If the resource is in the database and the credentials
-      # are valid, the user is authenticated.  Otherwise failure messages are returned 
+      # are valid, the user is authenticated.  Otherwise failure messages are returned
       # indicating whether the resource is not found in the database or the credentials
       # are invalid.
       def authenticate!
